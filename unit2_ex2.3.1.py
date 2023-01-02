26891d380ae73cf7b29fbeb358ce85ac34a135d4
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-02 08:57:08 +0200

    Create unit2_ex2.3.1.py

---
+++ unit2_ex2.3.1.py
@@ -0,0 +1,19 @@
+# In the program about the legend "The Ugly Duckling" we defined the following variables:
+fairy_tale = "The Ugly Duckling"
+swan_height = 90.7
+eggs_num = 4
+# What will be the output when you run the following lines of code?
+
+# 1:
+int(swan_height)
+# Enswer: 90
+
+# 2:
+# int(fairy_tale)
+# Enswer: error message
+
+# 3:
+surprise = bool(eggs_num)
+str(surprise)
+# Enswer: 'True'
+
