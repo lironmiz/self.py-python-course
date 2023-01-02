bc06a163157ee704fec0a5b960d56503660b598e
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-02 13:27:29 +0200

    Create unit4_ex4.4.1.py

---
+++ unit4_ex4.4.1.py
@@ -0,0 +1,26 @@
+# exercise 4.4.1 from unit 4
+# Match each piece of code with the output obtained when running it.
+
+# 1:
+temperature = float(input("What is the temperature? "))
+if temperature < 0:
+print("Brr... I need to find some penquins. to keep me company")
+  else:
+    print("Goodbye penquins!")
+# Enswer: error of kind IndentationError
+
+# 2:
+temperature = float(input("What is the temperature? "))
+if temperature < 0
+print("Brr... I need to find some penquins. to keep me company")
+else:
+    print("Goodbye penquins!")
+# Enswer: error of kind SyntaxError
+
+# 3:
+temperature = float(input("What is the temperature? "))
+if temperature < 0:
+print("Brr... I need to find some penquins. to keep me company")
+else:
+    print("Goodbye penquins!")
+# Enswer: what is the temperature?
