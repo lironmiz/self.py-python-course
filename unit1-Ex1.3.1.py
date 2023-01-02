189ef4e70564264102aacf0ae35788b4fbfa07e0
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-02 07:34:08 +0200

    Create unit1-Ex1.3.1.py

---
+++ unit1-Ex1.3.1.py
@@ -0,0 +1,35 @@
+''' Complete what the output will be obtained from running each line of code.
+If the line of code is incorrect, type "error"'''
+
+# 1:
+# print(hello world)
+# Enswer: error
+
+# 2:
+print(2 * 3)
+# Enswer: 6
+
+# 3:
+print(("3 * 3")
+# Enswer: 3 * 3
+      
+# 4:
+print(2, 2)
+# Enswer: 2 2
+     
+# 5:
+print("hello", "world")
+# Enswer: hello world
+      
+# 6:
+print("my age is", 7*3)
+# Enswer: my age is 21
+      
+# 7:
+print(15 // 2)
+# Enswer: my age is 7
+      
+# 8:
+print(50 + 2 * 4)
+# Enswer: my age is 58
+      
