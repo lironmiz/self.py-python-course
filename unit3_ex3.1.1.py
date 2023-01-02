064715f54cb394b7a3b9f8433165d6f7b95ce4c9
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-02 09:46:24 +0200

    Create unit3_ex3.1.1.py

---
+++ unit3_ex3.1.1.py
@@ -0,0 +1,5 @@
+# exercise 3.1.1 from unit 3
+# What will be the result of the following expression?
+output = "He" + "l" * 2 + "o" + " Python " + str(7.2 / 2) + "." + str(3)
+print(output)
+# Enswer: 'Hello Python 3.6.3'
