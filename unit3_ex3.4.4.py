782057334b1cc39d66705535b14aee171bf99442
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-02 12:49:27 +0200

    Create unit3_ex3.4.4.py

---
+++ unit3_ex3.4.4.py
@@ -0,0 +1,40 @@
+# exercise 3.4.4 from unit
+'''
+A programmer wrote a plan to recreate the launch of the "Apollo 11" spacecraft.
+
+The programmer forgot to write the code according to the spacing conventions
+used in Python. You need to fix the code snippet.
+
+Guidelines
+Copy each line of code into the answer box below.
+Add the necessary spaces to the given line of code.
+Do not change/delete any characters (the only action allowed is adding spaces).
+
+'''
+
+LAUNCH_DATE="16/07/1969"
+RETURN_DATE="24/07/1969"
+astronaut="Neil"+"Armstrong"
+days=how_many_days(LAUNCH_DATE‚RETURN_DATE)
+astronaut.take_to_space(days)
+
+# 1:
+# LAUNCH_DATE="16/07/1969"
+# Enswer: LAUNCH_DATE = "16/07/1969"
+
+# 2:
+# RETURN_DATE="24/07/1969"
+# Enswer: RETURN_DATE = "24/07/1969"
+
+# 3:
+# astronaut="Neil"+"Armstrong"
+# Enswer: astronaut = "Neil" + "Armstrong"
+
+# 4:
+# days=how_many_days(LAUNCH_DATE‚RETURN_DATE)
+# Enswer: days = how_many_days(LAUNCH_DATE‚ RETURN_DATE)
+
+# 5:
+# astronaut.take_to_space(days)
+# Enswer: astronaut.take_to_space(days)
+
