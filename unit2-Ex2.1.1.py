e379e078394f470321ee2b7ee5af39ab1e4edc25
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-02 07:57:49 +0200

    Create unit2-Ex2.1.1.py

---
+++ unit2-Ex2.1.1.py
@@ -0,0 +1,38 @@
+# Choose the type of each expression.
+# author - lirom mizrahi
+
+# 1:
+# Expression: 7
+type(7)
+# Enswer: int
+
+# 2:
+# Expression: "37"
+type("37")
+# Enswer: str
+
+# 3:
+# Expression: 37.00
+type(37.00)
+# Enswer: float
+
+# 4:
+# Expression: '34.56'
+type('34.56')
+# Enswer: str
+
+# 5:
+# Expression: 56.789
+type(56.789)
+# Enswer: float
+
+# 6:
+# Expression: "tfs56"
+type("tfs56")
+# Enswer: str
+
+# 7:
+# Expression: 3.7
+type(3.7)
+# Enswer: float
+
