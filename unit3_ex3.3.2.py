3901de2c7a5a3da22d2eb9636d5a5eaa5b2243a7
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-02 11:04:38 +0200

    Create unit3_ex3.3.2.py

---
+++ unit3_ex3.3.2.py
@@ -0,0 +1,21 @@
+# exercise 3.3.2 from unit 3
+
+'''
+Astronaut defined a string in a variable called msg (the string is a word from the field of astronomy). Unfortunately for the astronaut, the value of the msg variable was deleted for him.
+Your task: try to deduce, through the outputs of the commands in front of you, what the string in the msg variable is:
+
+Please note, the solution to the exercise does not require running the commands themselves (running will throw an error because the msg string is unknown)
+Put the string you discovered into the text box (without quotes).
+'''
+
+#>>> len(msg) 
+# 6
+# >>> msg[-1::]
+# 'y'
+# >>> msg[::2]
+# 'szg'
+# >>> msg[1] == msg[3] == msg[5]
+# True 
+
+# Enswer: syzygy
+
