46b08d13d92e1b3c9f3a265ea05af5f05eee5280
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-03 17:30:08 +0200

    Create unit5_ex5.3.2.py

---
+++ unit5_ex5.3.2.py
@@ -0,0 +1,10 @@
+# exercise 5.3.2 from unit 5
+
+# Here are several definitions of functions, mark all the functions that have a valid definition:
+
+# Answer
+
+def square_equation(a=1, b=0, c=0):
+  
+def square_equation(a, b=0, c=0):
+  
