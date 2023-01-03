73e974600d82392f8228499a6df8e33098c699e1
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-03 14:50:53 +0200

    Update unit2_ex2.2.1.py

--- unit2_ex2.2.1.py
+++ unit2_ex2.2.1.py
@@ -5,45 +5,45 @@
 # 1:
 true_or_false = "ron" != "ron"
 print(true_or_false)
-# Enswer: False
+# Answer: False
 
 # 2:
 true_or_false = "a" in "alpha"
 print(true_or_false)
-# Enswer: True
+# Answer: True
 
 # 3:
 true_or_false = "A" in "alpha"
 print(true_or_false)
-# Enswer: False
+# Answer: False
 
 # 4:
 true_or_false = "alpha" < "beta"
 print(true_or_false)
-# Enswer: True
+# Answer: True
 
 # 5:
 true_or_false = 1 < 2 < 1
 print(true_or_false)
-# Enswer: False
+# Answer: False
 
 # 6:
 true_or_false = 18 <= 22 < 30
 print(true_or_false)
-# Enswer: True
+# Answer: True
 
 # 7:
 true_or_false = not(10)
 print(true_or_false)
-# Enswer: False
+# Answer: False
 
 # 8:
 true_or_false = (7 > 10) and (1 > 1)
 print(true_or_false)
-# Enswer: False
+# Answer: False
 
 # 9:
 true_or_false = (20 % 10 == 2) or (1 == 1)
 print(true_or_false)
-# Enswer: True
+# Answer: True
 
