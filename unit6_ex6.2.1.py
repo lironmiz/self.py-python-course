d1ccd8fb28b906b3aef882fccc388fcd628bf9cf
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-03 19:50:25 +0200

    Create unit6_ex6.2.1.py

---
+++ unit6_ex6.2.1.py
@@ -0,0 +1,40 @@
+# exercise 6.2.1 from unit 6
+# there is 4 list 
+
+list1 = [1, 2, 3, 4]
+list2 = [5,6,7]
+list3 = list1 + list2
+list4 = [list1 + list2]
+
+# choose the Right output for each line of code
+
+# 1
+
+print(len(list3))
+       
+# Answer: 7
+       
+# 2
+
+print((len(list4)
+       
+# Answer: 1
+       
+# 3
+
+print(list1[2]+list3[4])
+       
+# Answer: 8
+       
+# 4
+
+print(list4[0][3])
+       
+# Answer: 4
+       
+# 5
+
+print(len(list2 * list1[1]))
+       
+# Answer: 6
+
