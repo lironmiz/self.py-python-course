4b2370b952ea7ac73ba5f0a7a86a53bfb6924c98
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-03 14:48:01 +0200

    fix typo

--- unit1-Ex1.3.1.py
+++ unit1-Ex1.3.1.py
@@ -3,33 +3,33 @@ If the line of code is incorrect, type "error"'''
 
 # 1:
 # print(hello world)
-# Enswer: error
+# Answer: error
 
 # 2:
 print(2 * 3)
-# Enswer: 6
+# Answer: 6
 
 # 3:
 print(("3 * 3")
-# Enswer: 3 * 3
+# Answer: 3 * 3
       
 # 4:
 print(2, 2)
-# Enswer: 2 2
+# Answer: 2 2
      
 # 5:
 print("hello", "world")
-# Enswer: hello world
+# Answer: hello world
       
 # 6:
 print("my age is", 7*3)
-# Enswer: my age is 21
+# Answer: my age is 21
       
 # 7:
 print(15 // 2)
-# Enswer: my age is 7
+# Answer: my age is 7
       
 # 8:
 print(50 + 2 * 4)
-# Enswer: my age is 58
+# Answer: my age is 58
       
