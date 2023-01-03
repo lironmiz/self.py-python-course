1aaa27b9cc5791ad297094d62a6a8c6cbf3e95bb
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-03 14:48:51 +0200

    Update unit2-Ex2.1.1.py

--- unit2-Ex2.1.1.py
+++ unit2-Ex2.1.1.py
@@ -4,35 +4,35 @@
 # 1:
 # Expression: 7
 type(7)
-# Enswer: int
+# Answer: int
 
 # 2:
 # Expression: "37"
 type("37")
-# Enswer: str
+# Answer: str
 
 # 3:
 # Expression: 37.00
 type(37.00)
-# Enswer: float
+# Answer: float
 
 # 4:
 # Expression: '34.56'
 type('34.56')
-# Enswer: str
+# Answer: str
 
 # 5:
 # Expression: 56.789
 type(56.789)
-# Enswer: float
+# Answer: float
 
 # 6:
 # Expression: "tfs56"
 type("tfs56")
-# Enswer: str
+# Answer: str
 
 # 7:
 # Expression: 3.7
 type(3.7)
-# Enswer: float
+# Answer: float
 
