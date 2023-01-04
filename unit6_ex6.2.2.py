7cf1f667de3608962d64e6b556aa37b173dd2b10
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-04 06:39:08 +0200

    Create unit6_ex6.2.2.py

---
+++ unit6_ex6.2.2.py
@@ -0,0 +1,17 @@
+# exersice 6.2.2 from unit 6
+
+'''
+given the list 
+programming_languages = ['Python', 'Perl', 'R', 'Ruby']
+
+which of the lines of code his output will be 'R'
+'''
+
+# Answer:
+print(programming_lanquages[2][0])
+print(programming_languages[3][0])
+print(programming_languages[-2][0])
+print(programming_languages[-1][-4])
+print(programming_languages[2])
+print(programming_languages[-2])
+                          
