f073de66fa4439f56847a60b761ddc7d796bc1d7
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-04 08:16:37 +0200

    Create unit7_ex7.1.2.py

---
+++ unit7_ex7.1.2.py
@@ -0,0 +1,16 @@
+# exercise 7.1.2 from unit 7
+'''
+Here is a piece of code that includes a while loop. What output is obtained when running the loop?
+
+Guidelines
+It is recommended to use a tracking table.
+'''
+
+x = 3
+i = 0
+while (i < 3):
+    x += 1
+    i += 1
+print(x)
+
+# Answer: 6
